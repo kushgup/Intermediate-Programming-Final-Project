78dbf522dd85c4967e2b2e44728f4fa073a3a475
Andrew Mao <andrewwmao@gmail.com>
2014-11-25 19:34:03 -0500

    Modified Player header files

--- Player.h
+++ Player.h
@@ -1,6 +1,37 @@
-#ifndef PLAYER_H_INCLUDED
-#define PLAYER_H_INCLUDED
+/*
+This class defines the abstract base class Player.
+It has a bunch of instance data that a player has, and functions pertaining to decision making the turn
+This class will be inherited by HumanPlayer and AIPlayer
+*/
 
+#ifndef _PLAYER_H
+#define _PLAYER_H
 
+class Player {
 
-#endif // PLAYER_H_INCLUDED
+protected:
+
+	String name; //store the name of the player
+	OList<Card> hand; //separate linked list of resource cards for the hand
+	OList<Card> camels; //separate linked list of camels
+	int points; //keep track of points, don't have a container to store tokens since we don't have a class for that
+	int seals; //keep track of how many seals the player has
+
+public:
+	
+	string getName() { return name; };
+	int handSize() { return hand.size(); };
+	int numCamels() { return camels.size(); };
+	virtual void makeMove() = 0; //called from Game, decides which action to take on the turn
+	
+protected:
+
+	void takeCamels(); //this method can be defined within Player itself since it will be the same for both human and ai players
+	void takeCard(); //option 1, take a single card from market
+	void exchange(); //option 2, take multiple cards from market and exchange from hand and/or camels
+	void sellCards(); //option 3, sell cards from the hand
+};
+
+//exchange, take, sell and takecamels are all the same for humans and computers, just the "decision" in "move" is different
+
+#endif
--- PlayerHuman.h
+++ PlayerHuman.h
@@ -1,6 +1,25 @@
-#ifndef PLAYERHUMAN_H_INCLUDED
-#define PLAYERHUMAN_H_INCLUDED
+/*
+This class defines the Human player, inherits from the abstract base class Player
+The only definitions in this file are overriding the virtual functions defined in Player.h
+*/
 
+#ifndef _HUMANPLAYER_H
+#define _HUMANPLAYER_H
 
+#include "Player.h"
 
-#endif // PLAYERHUMAN_H_INCLUDED
+class HumanPlayer: public Player {
+
+public:
+	
+	void makeMove(); //called from Game, decides which action to take on the turn
+
+private: //these are only called from makeMove
+
+	void takeCard(); //option 1, take a single card from market
+	void exchange(); //option 2, take multiple cards from market and exchange from hand and/or camels
+	void sellCards(); //option 3, sell cards from the hand
+	//note that all input and output from player will be implemented within these functions
+};
+
+#endif
