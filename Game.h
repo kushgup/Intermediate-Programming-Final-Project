9a0fe784a27382e1ccc59fb9ac05567551060fa7
Michael Miller <mmill179@jhu.edu>
2014-12-04 21:51:56 -0500

    failed

--- Game.h
+++ Game.h
@@ -20,7 +20,7 @@ private:
 public:
 
     Game();
-    Game(& Game);
+    Game(Game &);
     ~Game();
 
 	void printBoard();
@@ -31,6 +31,9 @@ public:
 	void createDeck();
 	void shuffleDeck();
 	void deal();
+
+private:
+    void setPlayers();
 
 };
 
