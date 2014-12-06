014d7c4da10cf36dd12815f622f94d4de7f1d003
Michael Miller <mmill179@jhu.edu>
2014-12-05 21:52:07 -0500

    fixed c & d for move
    q and 9 to finish

--- PlayerHuman.cpp
+++ PlayerHuman.cpp
@@ -18,12 +18,6 @@ void PlayerHuman::makeMove() {
 	//big try catch block to do exception handling
 	//try {
 		//have text-based GUI and construct move objects, then call individual functions checking valid first
-		
-
-		///////// if NOT using cin (using C-type functions instead) /////////
-		char c_one, c_two;
-		/////////////////////////////////////////////////////////////////////
-		
 
 		cin >> ch;
 		if(ch == 'a')
@@ -48,20 +42,30 @@ void PlayerHuman::makeMove() {
 			vector<int> hand_return;
 			int num_camels = 0;
 
-			while( isspace(c_one =getchar()) ); // c is the first non-whitespace character to be entered
-			while(c_one != '\n' && c_one != EOF)
-			{
+            cout << "-Exchange in market (q to finish): " << endl;
+            cout.flush();
+            char in_char;
+            cin >> in_char;
+            while (in_char != 'q') {
+                mkt_take.push_back(in_char);
+                cin >> in_char;
+            }
 
-				if(isdigit(c_one))
-					hand_return.push_back((int) c_one);
-				else if(c_one >= 'A' && c_one <= 'z')
-					mkt_take.push_back(c_one);
-				else if(c_one == '?')
-						num_camels++;
+            cout << "-Exchange in hand (9 to finish): " << endl;
+            cout.flush();
+            int in_hand;
+            cin >> in_hand;
+            while (in_hand != 9) {
+                hand_return.push_back(in_hand);
+                cin >> in_hand;
+            }
 
-				while( isspace(c_one =getchar()) ); // c is the first non-whitespace character to be entered
+            for( std::vector<char>::const_iterator i = mkt_take.begin(); i != mkt_take.end(); ++i)
+                std::cout << *i << ' ';
+            for( std::vector<int>::const_iterator i = hand_return.begin(); i != hand_return.end(); ++i)
+                std::cout << *i << ' ';
+            cout << endl;
 
-			}
 			Move next(game_field, hand, hand_return, mkt_take, num_camels, herd);
 			if(next.isValid())
                 exchange(next);
@@ -72,15 +76,19 @@ void PlayerHuman::makeMove() {
 			cout << "Selected d" << endl;
 			vector<int> to_sell;
 
-			while( isspace(c_two =getchar()) ); // c is the first non-whitespace character to be entered
-			while(c_two != '\n' && c_two != EOF)
-			{
-				if(isdigit(c_two))
-					to_sell.push_back((int) c_two);
+            cout << "-Cards in hand to sell (9 to finish): " << endl;
+            cout.flush();
+            int in_hand;
+            cin >> in_hand;
+            while (in_hand != 9) {
+                to_sell.push_back(in_hand);
+                cin >> in_hand;
+            }
 
-				while( isspace(c_one =getchar()) ); // c is the first non-whitespace character to be entered
+            for( std::vector<int>::const_iterator i = to_sell.begin(); i != to_sell.end(); ++i)
+                std::cout << *i << ' ';
+            cout << endl;
 
-			}
 			Move next(game_field, hand, to_sell);
 			if(next.isValid())
                 sellCards(next);
